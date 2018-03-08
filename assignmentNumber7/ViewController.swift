00029660e802c514b75c1af2517c6e8f05f8b11b
jaquawn <jd23@hawaii.edu>
2018-03-08 00:41:47 -1000

    assignment number 7 done

--- assignmentNumber7/ViewController.swift
+++ assignmentNumber7/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var displayTextLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.displayTextLabel.text=""
+        
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func setLabelButtonPressed(_ sender: Any) {
+        var userText: String!
+        userText = self.textField.text
+        self.displayTextLabel.text=userText
+    }
+    
+    
+    
 }
 
