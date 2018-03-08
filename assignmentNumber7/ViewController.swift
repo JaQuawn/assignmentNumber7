e5f49c143aa198f6c1b020603ff765aaaadba112
jaquawn <jd23@hawaii.edu>
2018-03-07 21:18:33 -1000

    Initial Commit

---
+++ assignmentNumber7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignmentNumber7
+//
+//  Created by Jaquawn Yarborough on 3/7/18.
+//  Copyright © 2018 Jaquawn Yarborough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
